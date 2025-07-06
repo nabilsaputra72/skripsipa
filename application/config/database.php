80ab3b422dfaed497f59e1ae7290331c522f79cb
nabilsaputra72 <nabilsaputra736@gmil.com>
2025-07-07 00:26:37 +0700

    ganti

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'pengaduan_user',
-        'password' => 'pengaduan2025',
+	'hostname' => 'localhost',
+	'username' => 'root',
+        'password' => ,
         'database' => 'dbpengaduan',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
